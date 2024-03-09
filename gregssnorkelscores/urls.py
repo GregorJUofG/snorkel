a675ae4d41ee2002e3fa035b280c39783243a764
greglikescode <gregorjohnston@icloud.com>
2024-03-09 15:54:11 +0000

    progress on linking all the pages together

--- gregssnorkelscores/urls.py
+++ gregssnorkelscores/urls.py
@@ -6,4 +6,10 @@ app_name = 'gregssnorkelscores'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/',views.about,name = 'about'),
+    path('location_name/',views.location,name = 'location'),
+    path('location_name/write_review/', views.write_review, name='write_review'),
+    path('profile/username/',views.profile,name = 'profile'),
+    path('add_location/',views.add_location,name = 'add_location'),
+    path('register/',views.register,name = 'register'),
+    path('login/',views.login,name = 'login'),
 ]
--- gregssnorkelscores/views.py
+++ gregssnorkelscores/views.py
@@ -1,6 +1,11 @@
-from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
 
 def home(request):
     context_dict = {}
@@ -21,7 +26,151 @@ def about(request):
     response = render(request, 'gregssnorkelscores/about.html', context=context_dict)
     return response
 
+def location(request):
+    context_dict = {}
+    context_dict['boldmessage'] = 'All the locations will be displayed in a list here!'
 
+    visitor_cookie_handler(request)
+
+    response = render(request, 'gregssnorkelscores/location.html', context=context_dict)
+    return response
+
+def add_location(request):
+    visitor_cookie_handler(request)
+
+    response = render(request, 'gregssnorkelscores/add_location.html')
+    return response
+
+
+def write_review(request):
+    visitor_cookie_handler(request)
+
+    response = render(request, 'gregssnorkelscores/write_review.html')
+    return response
+
+def profile(request):
+    visitor_cookie_handler(request)
+
+    response = render(request, 'gregssnorkelscores/profile.html')
+    return response
+
+def register(request):
+    
+    visitor_cookie_handler(request)
+
+    response = render(request, 'gregssnorkelscores/register.html')
+    return response
+
+    # HONGHUI's task
+
+    # # A boolean value for telling the template
+    # # whether the registration was successful.
+    # # Set to False initially. Code changes value to
+    # # True when registration succeeds.
+    # registered = False
+    # # If it's a HTTP POST, we're interested in processing form data.
+    # if request.method == 'POST':
+    #     # Attempt to grab information from the raw form information.
+    #     # Note that we make use of both UserForm and UserProfileForm.
+    #     user_form = UserForm(request.POST)
+    #     profile_form = UserProfileForm(request.POST)
+
+    #     # If the two forms are valid...
+    #     if user_form.is_valid() and profile_form.is_valid():
+    #         # Save the user's form data to the database.
+    #         user = user_form.save()
+
+    #         # Now we hash the password with the set_password method.
+    #         # Once hashed, we can update the user object.
+    #         user.set_password(user.password)
+    #         user.save()
+
+    #         # Now sort out the UserProfile instance.
+    #         # Since we need to set the user attribute ourselves,
+    #         # we set commit=False. This delays saving the model
+    #         # until we're ready to avoid integrity problems.
+    #         profile = profile_form.save(commit=False)
+    #         profile.user = user
+
+    #         # Did the user provide a profile picture?
+    #         # If so, we need to get it from the input form and
+    #         #put it in the UserProfile model.
+    #         if 'picture' in request.FILES:
+    #             profile.picture = request.FILES['picture']
+
+    #         # Now we save the UserProfile model instance.
+    #         profile.save()
+
+    #         # Update our variable to indicate that the template
+    #         # registration was successful.
+    #         registered = True
+    #     else:
+    #         # Invalid form or forms - mistakes or something else?
+    #         # Print problems to the terminal.
+    #         print(user_form.errors, profile_form.errors)
+    # else:
+    #     # Not a HTTP POST, so we render our form using two ModelForm instances.
+    #     # These forms will be blank, ready for user input.
+    #     user_form = UserForm()
+    #     profile_form = UserProfileForm()
+
+    # # Render the template depending on the context.
+    # return render(request,
+    #               'gregssnorkelscores/register.html',
+    #               context = {'user_form': user_form,
+    #                          'profile_form': profile_form,
+    #                          'registered': registered})
+
+def login(request):
+
+    visitor_cookie_handler(request)
+
+    response = render(request, 'gregssnorkelscores/login.html')
+    return response
+    # HONGHUI's task
+
+    # # If the request is a HTTP POST, try to pull out the relevant information.
+    # if request.method == 'POST':
+    #     # Gather the username and password provided by the user.
+    #     # This information is obtained from the login form.
+    #     # We use request.POST.get('<variable>') as opposed
+    #     # to request.POST['<variable>'], because the
+    #     # request.POST.get('<variable>') returns None if the
+    #     # value does not exist, while request.POST['<variable>']
+    #     # will raise a KeyError exception.
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+    #     # Use Django's machinery to attempt to see if the username/password
+    #     # combination is valid - a User object is returned if it is.
+    #     user = authenticate(username=username, password=password)
+    #     # If we have a User object, the details are correct.
+    #     # If None (Python's way of representing the absence of a value), no user
+    #     # with matching credentials was found.
+    #     if user:
+    #         # Is the account active? It could have been disabled.
+    #         if user.is_active:
+    #             # If the account is valid and active, we can log the user in.
+    #             # We'll send the user back to the homepage.
+    #             login(request, user)
+    #             return redirect(reverse('gregssnorkelscores:home'))
+    #         else:
+    #             # An inactive account was used - no logging in!
+    #             return HttpResponse("Your Rango account is disabled.")
+    #     else:
+    #         # Bad login details were provided. So we can't log the user in.
+    #         print(f"Invalid login details: {username}, {password}")
+    #         return HttpResponse("Invalid login details supplied.")
+    # # The request is not a HTTP POST, so display the login form.
+    # # This scenario would most likely be a HTTP GET.
+    # else:
+    #     # No context variables to pass to the template system, hence the
+    #     # blank dictionary object...
+    #     return render(request, 'gregssnorkelscores/login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('gregssnorkelscores:home'))
 
 def get_server_side_cookie(request, cookie, default_val=None):
     val = request.session.get(cookie)
@@ -36,7 +185,7 @@ def visitor_cookie_handler(request):
     '%Y-%m-%d %H:%M:%S')
 
     # If it's been more than a day since the last visit...
-    if (datetime.now() - last_visit_time).days > 0:
+    if (datetime.now() - last_visit_time).seconds > 0:
         visits = visits + 1
         # Update the last visit cookie now that we have updated the count
         request.session['last_visit'] = str(datetime.now())
