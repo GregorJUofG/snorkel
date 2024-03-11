967a4ea7486ccd69a11eb617c68dea22f9156d0a
Natasha Graham <2636245g@student.gla.ac.uk>
2024-03-11 19:02:38 +0000

    Just made a review model

--- gregssnorkelscores/admin.py
+++ gregssnorkelscores/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Review
+from .models import Location
+from .models import Spot
+from .models import UserProfile
 
 # Register your models here.
+admin.site.register(Review)
--- gregssnorkelscores/models.py
+++ gregssnorkelscores/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from django.conf import settings
+#from catalog.models import Spots  copied from website idk yet what our equivalent of catalog is
+
+
+###############
+# REVIEWS
+# https://michaelstromer.nyc/books/intro-to-django/django-reviews
+#################
+
 
 class Location(models.Model):
     # we will want location names to be unique 
@@ -19,7 +28,7 @@ class Location(models.Model):
         return self.name
 
 
-class Spots(models.Model):
+class Spot(models.Model):
     ## think this is right
     ## cpoied how category and page are connected
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
@@ -34,6 +43,31 @@ class Spots(models.Model):
     # reviews = ??
     # author = ??
 
+
+class Review(models.Model):
+
+    # 1-5 stars
+    RATING_CHOICES = (
+        (5, '5'),
+        (4, '4'),
+        (3, '3'),
+        (2, '2'),
+        (1, '1'),
+    )
+
+    spot = models.ForeignKey(Spot, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Publication Date')
+    comment = models.TextField(max_length=500)
+    value = models.IntegerField(choices=RATING_CHOICES, default=1)
+
+    def __str__(self):
+        return '{0}/{1} - {2}'.format(self.book.title, self.user.username, self.value)
+    
+    class Meta:
+        verbose_name = "Spot Review"
+        verbose_name_plural = "Spot Reviews" #wont need?
+        ordering = ['-pub_date']
 
 
 
