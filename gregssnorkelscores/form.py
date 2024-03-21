fcc1060858583c2984302a513f42f5ab7f12dfbe
Honghui Yang <2841006y@student.gla.ac.uk>
2024-03-21 16:39:11 +0000

    add experience box

--- gregssnorkelscores/form.py
+++ gregssnorkelscores/form.py
@@ -47,4 +47,4 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('website', 'pictures',)
+        fields = ('website', 'pictures', 'experience')
